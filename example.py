062e9acf2b557f65d0be5aa80b65fcb451e0a903
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-04-09 12:34:03 -0500

    moved main function to example.py, since this was just an example on how to use the lib

---
+++ example.py
@@ -0,0 +1,33 @@
+from chirpstack_api_wrapper import *
+
+def main(): # pragma: no cover
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", help="enable debug logs")
+    parser.add_argument(
+        "--chirpstack-account-email",
+        default=os.getenv("CHIRPSTACK_ACCOUNT_EMAIL"),
+        help="The Chirpstack Account's email to use to access APIs",
+    )
+    parser.add_argument(
+        "--chirpstack-account-password",
+        default=os.getenv("CHIRPSTACK_ACCOUNT_PASSWORD"),
+        help="The Chirpstack Account's password to use to access APIs",
+    )
+    parser.add_argument(
+        "--chirpstack-api-interface",
+        default=os.getenv("CHIRPSTACK_API_INTERFACE"),
+        help="Chirpstack's server API interface. The port is usually 8080",
+    )
+    args = parser.parse_args()
+    #configure logging
+    logging.basicConfig(
+        level=logging.DEBUG if args.debug else logging.INFO,
+        format="%(asctime)s %(message)s",
+        datefmt="%Y/%m/%d %H:%M:%S",
+    )
+    chirpstack_client = ChirpstackClient(args)
+
+    chirpstack_client.list_tenants()
+
+if __name__ == "__main__":
+    main() # pragma: no cover

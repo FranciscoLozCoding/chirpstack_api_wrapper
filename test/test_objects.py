cd1ff78d9c8ae5447c7ee1ff0f13389f9c5a4e38
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-04-15 15:54:20 -0500

    added test for Application object

--- test/test_objects.py
+++ test/test_objects.py
@@ -25,5 +25,39 @@ class TestGateway(unittest.TestCase):
         # Assertations
         self.assertEqual(str(mock_gateway), "mock_gw_id")
 
+class TestApplication(unittest.TestCase):
+    def test_tags_ValueError(self):
+        """
+        Test App's tags ValueError in init 
+        """
+        mock_tags = {
+            "mock1": 1, # NOT a str
+            "mock2": "test2"
+        }
+        # Init Application and Assert Raise
+        with self.assertRaises(ValueError) as context:
+            Application("mock_name","mock_tenant_id",tags=mock_tags)
+
+    def test_str_method(self):
+        """
+        Test Apps's conversion to string
+        """
+        mock_app = Application("mock_name","mock_tenant_id",id="mock_id")
+
+        # Assertations
+        self.assertEqual(str(mock_app), "mock_id")
+
+    def test_str_method_no_id(self):
+        """
+        Test Apps's conversion to string when the id is empty
+        """
+        mock_app = Application("mock_name","mock_tenant_id")
+
+        #Assert Raise
+        with self.assertRaises(RuntimeError) as context:
+            str(mock_app)
+
+
+
 if __name__ == "__main__":
     unittest.main()
